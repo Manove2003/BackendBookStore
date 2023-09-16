6043d6ebdfbe283d8ef6aa9761881fab8ebae59d
Manove Kumar <kumarmanove2003.gmail.com>
2023-09-16 23:05:54 +0500

    updae successful

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require("./connection/connection")
 app.use(express.json());
 app.use(cors())
 app.use("/api/v1",bookRoutes);
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT || 1004,()=>{
     console.log("SERVER STARTED SUCCESSFULLY");
 });
 
